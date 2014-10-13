952917c712de0141d0f817ea0d55b65924918741
albert <albertxerta93@hotmail.com>
2014-10-13 17:49:49 +0200

    13/10/14 .1

---
+++ application/config/codeigniterhelloworld.php
@@ -0,0 +1,14 @@
+<?php if ( !defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|------------------------------------------------------------------------
+|Periode acadèmic
+|------------------------------------------------------------------------
+|blablabla
+|
+*/
+
+$config['academic_period'] = "2014-15";
+
+/*End of the file codeigniterhelloworld.php */
+/* Location: ./application/config/codeigniterhelloworld.php */
